53973a9028cd284e6c4722a656865de480a101e5
Janie Amero <janie.amero@gmail.com>
2019-04-08 17:33:30 +0100

    got latitude to appear

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 // import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // this is the only time you can directly assign this.state
+    this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // always call setState!!!
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log(err)
+    );
+  }
+  // React says we have to define render!
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
