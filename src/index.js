0911a923d346a89dbd220c33a0cd34722216156c
Janie Amero <janie.amero@gmail.com>
2019-04-09 23:17:57 +0100

    completed simple app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -12,16 +13,18 @@ class App extends React.Component {
     );
   }
 
-  // React says we have to define render!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}/>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
+    return <Spinner message="Please accept location request" />;
+  }
 
-    return <div>Loading!</div>;
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
